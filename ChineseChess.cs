cb9b589187e798e2a07d683128bbbee2055b8aea
codie <ngnhathuy1224@gmail.com>
2023-04-21 18:37:18 +0700

    init base

---
+++ ChineseChess.cs
@@ -0,0 +1,93 @@
+﻿using ChineseChessGame.instances;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ChineseChessGame
+{
+    public class ChineseChess : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private const int WindowWidth = 1600, WindowHeight = 900;
+
+        public KeyboardState kb;
+        public MouseState mouse;
+
+        private Texture2D background;
+        private Rectangle backgroundRect;
+
+        private Piece piece;
+
+        public ChineseChess()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // GRAPHICS SETUP
+            graphics.PreferredBackBufferWidth = WindowWidth;
+            graphics.PreferredBackBufferHeight = WindowHeight;
+            graphics.ApplyChanges();
+
+            // WINDOW SETUP
+            Window.AllowUserResizing = false;
+            Window.AllowAltF4 = true;
+            Window.Title = "Chinese Chess Game";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            background = Content.Load<Texture2D>("textures/background");
+            backgroundRect = new Rectangle(0, 0, WindowWidth, WindowHeight);
+
+            piece = new Piece(Content.Load<Texture2D>("sprites/player"));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            kb = Keyboard.GetState();
+
+            if (kb.IsKeyDown(Keys.Escape)) this.Exit();
+
+            piece.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkGray);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            //Start
+
+            spriteBatch.Draw(background, backgroundRect, Color.DarkGray);
+
+            piece.Draw(spriteBatch);
+
+            //End
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ instances/Piece.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseChessGame.instances
+{
+    internal class Piece
+    {
+        protected MouseState mouse;
+
+        protected Texture2D piece;
+        protected Rectangle pieceRect;
+
+        protected const int PieceWidth = 25, PieceHeight = 25; 
+
+        public Piece(Texture2D piece)
+        {
+            this.piece = piece;
+            this.pieceRect = new Rectangle(50, 50, PieceWidth, PieceHeight);
+        }
+
+
+
+        public void Update()
+        {
+            mouse = Mouse.GetState();
+
+            if (this.isMouseOnPiece(mouse)) {
+                if (mouse.LeftButton == ButtonState.Pressed)
+                {
+                    pieceRect.X += 5;
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(piece, pieceRect, Color.White);
+        }
+
+        protected Boolean isMouseOnPiece(MouseState mouse)
+        {
+            return (mouse.X >= pieceRect.X && mouse.X < pieceRect.X + PieceWidth) &&
+                (mouse.Y >= pieceRect.Y && mouse.Y < pieceRect.Y + PieceHeight);
+        }
+
+        protected void highlightBorder()
+        {
+            
+        }
+    }
+}
