f0298629e301cbab40fbc3de1283267bdf261a35
codie <ngnhathuy1224@gmail.com>
2023-04-21 21:49:41 +0700

    feat: add draw-board-border

--- ChineseChess.cs
+++ ChineseChess.cs
@@ -2,6 +2,9 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace ChineseChessGame
 {
@@ -11,12 +14,27 @@ namespace ChineseChessGame
         private SpriteBatch spriteBatch;
 
         private const int WindowWidth = 1600, WindowHeight = 900;
+        private const int BoardMarginLeft = 550, BoardMarginTop = 100;
+        private const int PiecesGap = 80;
 
-        public KeyboardState kb;
-        public MouseState mouse;
+        private const int BoardWidth = PiecesGap * 8, BoardHeight = PiecesGap * 9;
+
+        private KeyboardState kb;
+        private MouseState mouse;
+
+        private Song bgm;
 
         private Texture2D background;
         private Rectangle backgroundRect;
+
+        private SpriteFont textFont;
+
+        private Texture2D line;
+
+        private const string banner = "Chinese Chess Game UwU";
+        private Vector2 bannerPos;
+
+        private Texture2D pieceBorder;
 
         private Piece piece;
 
@@ -53,7 +71,19 @@ namespace ChineseChessGame
             background = Content.Load<Texture2D>("textures/background");
             backgroundRect = new Rectangle(0, 0, WindowWidth, WindowHeight);
 
-            piece = new Piece(Content.Load<Texture2D>("sprites/player"));
+            bgm = Content.Load<Song>("audio/bgm");
+            MediaPlayer.Play(bgm);
+            MediaPlayer.IsRepeating = true;
+
+            line = new Texture2D(graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            line.SetData<Color>(new[] { Color.White });
+
+            textFont = Content.Load<SpriteFont>("fonts/textFont");
+            bannerPos = new Vector2(10, 10);
+
+            pieceBorder = Content.Load<Texture2D>("textures/border");
+
+            this.initPieces();
         }
 
         protected override void Update(GameTime gameTime)
@@ -67,6 +97,9 @@ namespace ChineseChessGame
 
             if (kb.IsKeyDown(Keys.Escape)) this.Exit();
 
+            bannerPos.X += 2;
+            if (bannerPos.X > WindowWidth) bannerPos.X = 0;
+
             piece.Update();
 
             base.Update(gameTime);
@@ -78,16 +111,66 @@ namespace ChineseChessGame
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            //Start
+            //Start Drawing
 
             spriteBatch.Draw(background, backgroundRect, Color.DarkGray);
 
+            spriteBatch.DrawString(textFont, banner, bannerPos, Color.White);
+
+            this.drawBoardBorder();
+
             piece.Draw(spriteBatch);
 
-            //End
+            //End Drawing
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void initPieces()
+        {
+            piece = new Piece(Content.Load<Texture2D>("sprites/player"), pieceBorder);
+        }
+
+        private void drawBoardBorder()
+        {
+            for (int i = 0; i != 10; i++)
+            {
+                int x = BoardMarginLeft, y = BoardMarginTop + PiecesGap * i;
+                this.drawLine(spriteBatch, new Vector2(x, y), new Vector2(x + BoardWidth, y));
+            }
+
+            for (int i = 0; i != 9; i++)
+            {
+                int x = BoardMarginLeft + PiecesGap * i, y1 = BoardMarginTop, y2 = BoardMarginTop + PiecesGap * 5;
+
+                if (i == 0 || i == 8) this.drawLine(spriteBatch, new Vector2(x, y1), new Vector2(x, y1 + 5 * PiecesGap));
+                else this.drawLine(spriteBatch, new Vector2(x, y1), new Vector2(x, y1 + 4 * PiecesGap));
+
+                this.drawLine(spriteBatch, new Vector2(x, y2), new Vector2(x, y2 + 4 * PiecesGap));
+            }
+        }
+
+        void drawLine(SpriteBatch sb, Vector2 start, Vector2 end)
+        {
+            Vector2 edge = end - start;
+
+            float angle =
+                (float)Math.Atan2(edge.Y, edge.X);
+
+            sb.Draw(this.line,
+                new Rectangle(
+                    (int)start.X,
+                    (int)start.Y,
+                    (int)edge.Length(),
+                    5),
+                null,
+                Color.DarkBlue,
+                angle,
+                new Vector2(0, 0),
+                SpriteEffects.None,
+                0);
+
         }
     }
 }
--- instances/Piece.cs
+++ instances/Piece.cs
@@ -16,29 +16,47 @@ namespace ChineseChessGame.instances
         protected Texture2D piece;
         protected Rectangle pieceRect;
 
-        protected const int PieceWidth = 25, PieceHeight = 25; 
+        protected Texture2D border;
+        protected Rectangle borderRect;
 
-        public Piece(Texture2D piece)
+        protected const int PieceWidth = 50, PieceHeight = 50;
+
+        protected Boolean hasHighlightBorder = true;
+
+        public Piece(Texture2D piece, Texture2D border)
         {
             this.piece = piece;
             this.pieceRect = new Rectangle(50, 50, PieceWidth, PieceHeight);
+
+            this.border = border;
+            this.borderRect = new Rectangle(0, 0, PieceWidth + 4, PieceHeight + 4);
         }
-
-
 
         public void Update()
         {
             mouse = Mouse.GetState();
 
             if (this.isMouseOnPiece(mouse)) {
+                this.hasHighlightBorder = true;
+                this.createBorder();
+
                 if (mouse.LeftButton == ButtonState.Pressed)
                 {
                     pieceRect.X += 5;
                 }
             }
+            else
+            {
+                this.hasHighlightBorder = false;
+            }
         }
         public void Draw(SpriteBatch spriteBatch)
         {
+            if (this.hasHighlightBorder)
+            {
+                spriteBatch.Draw(border, borderRect, Color.White);
+            }
+            
             spriteBatch.Draw(piece, pieceRect, Color.White);
         }
 
@@ -48,9 +66,10 @@ namespace ChineseChessGame.instances
                 (mouse.Y >= pieceRect.Y && mouse.Y < pieceRect.Y + PieceHeight);
         }
 
-        protected void highlightBorder()
+        protected void createBorder()
         {
-            
+            this.borderRect.X = this.pieceRect.X - 2;
+            this.borderRect.Y = this.pieceRect.Y - 2;
         }
     }
 }
