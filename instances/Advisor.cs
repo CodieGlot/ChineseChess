5d0eaf21bb71bbfa2062d953cfd461e1919381e2
Le Minh Toan <22521489@gm.uit.edu.vn>
2023-04-25 13:13:33 +0700

    General move

--- instances/Advisor.cs
+++ instances/Advisor.cs
@@ -18,8 +18,9 @@ namespace ChineseChessGame.instances
         {
             this.validMoves = new List<int[]>();
 
-            if (X < 5)
-             {
+            /*if (X < 5)
+            {
+
                     if (this.isValidMove(board, X + 1, Y + 1))
                     {
                         this.validMoves.Add(new int[] { X + 1, Y + 1 });
@@ -29,8 +30,8 @@ namespace ChineseChessGame.instances
                     {
                         this.validMoves.Add(new int[] { X + 1, Y - 1 });
                     }
-                }
-                
+            }
+ 
                 if(X>3)
                 {
                     if(this.isValidMove(board, X - 1, Y - 1))
@@ -42,9 +43,11 @@ namespace ChineseChessGame.instances
                         this.validMoves.Add(new int[] { X - 1, Y + 1 });
                     }
                 }
+            */
+            if (team == Team.BLACK)
+            {
+
             }
-
-
-      
+        } 
     }
 }
---
+++ instances/General.cs
@@ -0,0 +1,66 @@
+﻿using ChineseChessGame.constants;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseChessGame.instances
+{
+    internal class General : Piece
+    {
+
+        public General(Texture2D piece, Texture2D border, Team team)
+           : base(piece, border, team)
+        { }
+        protected override void assignValidMoves(Piece[,] board)
+        {
+
+            this.validMoves = new List<int[]>();
+            if (this.team == Team.BLACK) {
+                int XValidMin = 3, XValidMax = 5, YValidMin = 0, YValidMax = 2;
+                if (YValidMin <= Y - 1 && Y - 1 <= YValidMax && this.isValidMove(board, X, Y - 1))
+                {
+                    this.validMoves.Add(new int[] { X, Y - 1 });
+                }
+                if (YValidMin <= Y + 1 && Y + 1 <= YValidMax && this.isValidMove(board, X, Y + 1))
+                {
+                    this.validMoves.Add(new int[] { X, Y + 1 });
+                }
+                if (XValidMin <= X - 1 && X - 1 <= XValidMax && this.isValidMove(board, X - 1, Y))
+                {
+                    this.validMoves.Add(new int[] { X - 1, Y });
+                }
+                if (XValidMin <= X + 1 && X + 1 <= XValidMax && this.isValidMove(board, X + 1, Y))
+                {
+                    this.validMoves.Add(new int[] { X + 1, Y });
+                }
+            }
+            else if(this.team==Team.RED)
+            {
+                this.validMoves.Add(new int[] { 0, 0 });
+                this.validMoves.Add(new int[] { 4, 8 });
+                /*
+                int XValidMin = 3, XValidMax = 5, YValidMin = 7, YValidMax = 9;
+                if (YValidMin <= Y - 1 && Y - 1 <= YValidMax && this.isValidMove(board, X, Y - 1))
+                {
+                    this.validMoves.Add(new int[] { X, Y - 1 });
+                }
+                if (YValidMin <= Y + 1 && Y + 1 <= YValidMax && this.isValidMove(board, X, Y + 1))
+                {
+                    this.validMoves.Add(new int[] { X, Y + 1 });
+                }
+                if (XValidMin <= X - 1 && X - 1 <= XValidMax && this.isValidMove(board, X - 1, Y))
+                {
+                    this.validMoves.Add(new int[] { X - 1, Y });
+                }
+                if (XValidMin <= X + 1 && X + 1 <= XValidMax && this.isValidMove(board, X + 1, Y))
+                {
+                    this.validMoves.Add(new int[] { X + 1, Y });
+                }
+                */
+            }
+        }
+    }
+}
